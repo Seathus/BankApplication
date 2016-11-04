3a35d652b4005bc92ab0a40c3b6e3444631bf566
Tim Ogunmakin <timogunmakin@gmail.com>
2016-11-04 22:13:57 +0000

    Update CurrentAccount.java

--- JavaApplication1/src/BankApplication/CurrentAccount.java
+++ JavaApplication1/src/BankApplication/CurrentAccount.java
@@ -29,14 +29,15 @@ public class CurrentAccount extends Account
     void Withdraw(float amount)
     {
         float overdraftFee = 0;
-        float withdrawFee = amount * 0.05f;        
+        float withdrawFee = amount * 0.05f;
+        charges += withdrawFee + overdraftFee;
+        balance -= amount;
         if (balance <= 0)
         {
             System.out.println("You are in overdraft!");
             overdraftFee = amount * 0.5f;
         }
-        charges += withdrawFee + overdraftFee;
-        balance -= amount;
+        
     }
     
     @Override
